3bc78ba983d5351f08091435668fabedc2f42644
efortuna <emily@temporal.io>
2023-04-25 10:59:29 -0700

    Revert "Update python/workflows.py"
    This reverts commit 05c4646cb9e82d30eeb57c2887de0f0ab10d4c78.

--- python/workflows.py
+++ python/workflows.py
@@ -33,15 +33,19 @@ class Compensations:
     async def compensate(self):
         if self.parallel_compensations:
 
-            async def run_compensation(compensation: typing.Callable[..., typing.Awaitable[None]]) -> None:
-                try:
-                    await workflow.execute_activity(
-                        compensation,
-                        start_to_close_timeout=time_delta,
-                        retry_policy=common_retry_policy,
-                    ))
-                except Exception as e:
-                    workflow.logger.exception("failed to compensate")
+            def compensation_lambda(compensation):
+                task = asyncio.create_task(workflow.execute_activity(
+                    compensation,
+                    start_to_close_timeout=time_delta,
+                    retry_policy=common_retry_policy,
+                ))
+
+                def err_callback(task):
+                    if task.error():
+                        workflow.logger("Failed to compensate: %s" % task.error())
+                # Used low-level API so that errors are returned as they are encountered.
+                task.add_done_callback(err_callback) 
+                return task
 
             all_compensations = [compensation_lambda(c) for c in self.compensations]
             await asyncio.gather(*all_compensations)
