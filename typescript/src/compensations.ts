2260a4577100ad065bb50d0c8563b070b13dd889
efortuna <emily@temporal.io>
2023-05-02 09:11:46 -0700

    fix TS bug

--- typescript/src/compensations.ts
+++ typescript/src/compensations.ts
@@ -4,14 +4,19 @@ export type Compensation = () => Promise<void>
 
 export async function compensate(compensations: Compensation[], compensateInParallel = false) {
   if (compensateInParallel) {
-    compensations.map(comp => comp().catch(err => console.error(`failed to compensate: $error`)))
-  } else {
-    for (const comp of compensations) {
-      try {
-        await comp()
-      } catch (err) {
-        console.error(`failed to compensate: ${err}`)
-      }
+    await Promise.allSettled(
+      compensations.map((comp) =>
+        comp().catch((err) => console.error(`failed to compensate: $error`))
+      )
+    )
+    return
+  }
+
+  for (const comp of compensations) {
+    try {
+      await comp()
+    } catch (err) {
+      console.error(`failed to compensate: ${err}`)
     }
   }
 }
